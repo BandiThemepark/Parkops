39d5e0b55f6b4e49a5fc63360cbf5f42fa4213fc
Gezelligheid <elias6503@gmail.com>
2024-09-30 21:46:41 +0200

    loginPage

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,52 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Button from "@/components/ui/button/Button.vue";
+import Input from "@/components/ui/input/Input.vue";
+</script>
 
 <template>
-  <h1>Loginscreen</h1>
+  <main class="w-full h-screen flex">
+    <aside
+      class="w-full h-full hidden lg:flex bg-stone-900 p-8 text-white flex-col justify-between"
+    >
+      <h1 class="text-2xl font-bold mb-2">ParkOps</h1>
+      <div>
+        <q class="text-4xl opacity-70">
+          Innovation is the thrill of our rollercoaster, always pushing
+          boundaries with new twists and turns. At
+          <span class="font-semibold">BandiThemepark</span>, every ride is a
+          fresh adventure in creativity.
+        </q>
+      </div>
+    </aside>
+    <div
+      class="w-full h-full flex justify-center items-center bg-white p-4 sm:p-6"
+    >
+      <div class="w-full sm:w-[350px]">
+        <div class="text-center mb-6">
+          <h2 class="font-bold text-2xl">Login to your account</h2>
+          <p class="text-sm text-stone-500">
+            Just to make sure you are not an imposter
+          </p>
+        </div>
+        <div class="grid gap-2 mb-4">
+          <Input type="email" placeholder="john.doe@gmail.com" />
+          <Input type="password" placeholder="P@ssw0rd" />
+          <Button>Log in</Button>
+        </div>
+        <div class="flex items-center text-stone-300 space-x-4 mb-4">
+          <div class="w-full h-px bg-stone-300"></div>
+          <p>or</p>
+          <div class="w-full h-px bg-stone-300"></div>
+        </div>
+        <div class="text-center w-full">
+          <a
+            class="underline underline-offset-2 text-stone-500"
+            href="https://forms.gle/YPSPM95vLwF5BZB58"
+            target="_blank"
+            >Become a crewmember!</a
+          >
+        </div>
+      </div>
+    </div>
+  </main>
 </template>

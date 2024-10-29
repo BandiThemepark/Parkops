26b53dfa06e634a9622c8ecf9ccab6e298dbabeb
PartyProNL <63144876+PartyProNL@users.noreply.github.com>
2024-10-29 18:56:39 +0100

    Add second part of create cosmetic sheet

--- src/views/cosmetics/CreateCosmeticSheet.vue
+++ src/views/cosmetics/CreateCosmeticSheet.vue
@@ -14,20 +14,20 @@
         <SheetDescription>Enter all of the details below. You can find more information about creating cosmetics <a class="underline text-cyan-400 visited:text-cyan-400" href="https://docs.bandithemepark.net">here.</a></SheetDescription>
       </SheetHeader>
 
-      <div class="py-4 gap-2 grid">
+      <div class="py-4 gap-2 grid" v-if="currentPage === 1">
         <div>
           <Label for="email" class="text-sm font-medium">Name</Label>
-          <Input id="email" type="email" placeholder="bag_of_bricks" />
+          <Input id="email" type="text" placeholder="bag_of_bricks" />
         </div>
 
         <div>
           <Label for="email" class="text-sm font-medium">Display Name</Label>
-          <Input id="email" type="email" placeholder="Bag of Bricks" />
+          <Input id="email" type="text" placeholder="Bag of Bricks" />
         </div>
 
         <div>
           <Label for="email" class="text-sm font-medium">Description</Label>
-          <Input id="email" type="email" placeholder="Line 1&&and line 2" />
+          <Input id="email" type="text" placeholder="Line 1&&and line 2" />
         </div>
 
         <NumberField :default-value="25" :min="0">
@@ -40,10 +40,47 @@
         </NumberField>
       </div>
 
-      <SheetFooter>
-        <SheetClose as-child>
-          <Button>Save changes</Button>
-        </SheetClose>
+      <SheetFooter v-if="currentPage === 1">
+        <Button @click="currentPage++">Next step</Button>
+      </SheetFooter>
+
+      <div class="py-4 gap-2 grid" v-if="currentPage === 2">
+        <div>
+          <Label for="type" class="text-sm font-medium">Type</Label>
+          <Select v-model="selectedType" id="type">
+            <SelectTrigger>
+              <SelectValue placeholder="Select a type" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Cosmetic types</SelectLabel>
+                <SelectItem v-for="type in types" :value="type.name">
+                  {{type.displayName}}
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div>
+          <Label for="metadata" class="text-sm font-medium">Metadata</Label>
+          <Textarea v-model="metadata" id="metadata" type="text" placeholder="Select a type for example metadata" />
+        </div>
+
+        <div>
+          <Label for="requirements" class="text-sm font-medium">Requirements</Label>
+          <Textarea default-value="[]" id="requirements" type="text" placeholder="Enter requirements here" />
+        </div>
+      </div>
+
+      <SheetFooter v-if="currentPage === 2">
+        <div class="flex justify-between w-full">
+          <Button variant="outline" @click="currentPage--">Previous step</Button>
+
+          <SheetClose as-child>
+            <Button>Complete</Button>
+          </SheetClose>
+        </div>
       </SheetFooter>
     </SheetContent>
   </Sheet>
@@ -69,4 +106,36 @@ import {
   NumberFieldIncrement,
   NumberFieldInput
 } from "@/components/ui/number-field";
+import {ref, watch} from "vue";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select";
+import {Textarea} from "@/components/ui/textarea";
+
+const currentPage = ref(1);
+
+const types = ref([
+  {name: "hat", displayName: "Hat", tabName: "Hats", defaultMetadata: `{"material":"LEATHER_HORSE_ARMOR","customModelData":0,"heightOffset":0.0}`},
+  {name: "handheld", displayName: "Handheld", tabName: "Handhelds", defaultMetadata: `{"customModelData":0}`},
+  {name: "balloon", displayName: "Balloon", tabName: "Balloons", defaultMetadata: `{}`},
+  {name: "title", displayName: "Title", tabName: "Titles", defaultMetadata: `{"text":"<#FFFFFF>Your text"}`},
+  {name: "kart", displayName: "Kart", tabName: "Karts", defaultMetadata: `{}`},
+  {name: "backpack", displayName: "Backpack", tabName: "Backpacks", defaultMetadata: `{"customModelData":0}`},
+  {name: "chestplate", displayName: "Chestplate", tabName: "Chestplates", defaultMetadata: `{}`},
+  {name: "leggings", displayName: "Leggings", tabName: "Leggings", defaultMetadata: `{}`},
+  {name: "boots", displayName: "Boots", tabName: "Boots", defaultMetadata: `{}`},
+])
+
+const selectedType = ref()
+watch(selectedType, () => {
+  metadata.value = types.value.find(t => t.name === selectedType.value)?.defaultMetadata!
+})
+
+const metadata = ref("")
 </script>

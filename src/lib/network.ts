7efdf7a1b6e8fd0d120b3ff77100d07ba1d9e0ec
Gezelligheid <elias6503@gmail.com>
2024-11-04 17:01:16 +0100

    Edited develop - production backend url

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -1,5 +1,7 @@
 import useAuthentication from "../middleware/authentication";
 
-const BANDITHEMEPARK_API = "https://api.bandithemepark.net/";
+const BANDITHEMEPARK_API = import.meta.env.PROD
+  ? "https://api.bandithemepark.net/"
+  : "http://localhost:3000/";
 // const BANDITHEMEPARK_API = "http://localhost:3000/";
 export default BANDITHEMEPARK_API;

7b76c0b7e86aa5cbc7cea6b001192af40f181dcd
Gezelligheid <elias6503@gmail.com>
2024-11-12 20:32:24 +0100

    Woopps

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -1,6 +1,6 @@
 import useAuthentication from "../middleware/authentication";
 
-const BANDITHEMEPARK_API = !import.meta.env.PROD
+const BANDITHEMEPARK_API = import.meta.env.PROD
   ? "https://api.bandithemepark.net/"
   : "http://localhost:3000/";
 // const BANDITHEMEPARK_API = "http://localhost:3000/";

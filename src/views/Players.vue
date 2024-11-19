198041afa6857e5257906c6671cd90d407b68d38
Gezelligheid <elias6503@gmail.com>
2024-11-19 19:03:35 +0100

    Thanks prettier

--- src/views/Players.vue
+++ src/views/Players.vue
@@ -3,7 +3,6 @@ import { ref, h } from "vue";
 import DashboardProvider from "../components/custom/DashboardProvider.vue";
 import { getAllPlayers } from "../lib/backend/players";
 import DynaTable from "@/components/custom/dynatable/DynaTable.vue";
-import { playerColums } from "@/components/custom/dynatable/scenarioSpecific/players/playerColumns";
 
 import {
   CrownIcon,
@@ -22,6 +21,7 @@ import CardTitle from "@/components/ui/card/CardTitle.vue";
 import CardDescription from "@/components/ui/card/CardDescription.vue";
 import CardContent from "@/components/ui/card/CardContent.vue";
 import Badge from "@/components/ui/badge/Badge.vue";
+import { playerColums } from "@/components/custom/dynatable/scenarioSpecific/players/playerColumns";
 
 const playerData = ref([]);
 const isLoading = ref(false);
@@ -75,82 +75,81 @@ const secondsToNiceTime = (seconds: number) => {
         },
       ]"
       v-slot="{ rowData }"
+      :has-extended-row="true"
     >
-      <template name="expandable">
-        <div class="">
-          <Card>
-            <CardHeader>
-              <CardTitle> Online time </CardTitle>
-              <CardDescription>
-                {{ secondsToNiceTime(rowData.playtime || 0) }}
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <div class="w-full flex h-8 rounded-lg overflow-hidden">
-                <div
-                  class="bg-stone-700 h-full flex items-center pl-2"
-                  :style="{
-                    width:
-                      (rowData.playtime || 0) + (rowData.afkTime || 0) === 0
-                        ? '50%'
-                        : (((rowData.playtime || 0) - (rowData.afkTime || 0)) /
-                            (rowData.playtime || 0)) *
-                            100 +
-                          '%',
-                  }"
-                >
-                  <span class="truncate text-accent font-medium">
-                    Active:
-                    {{
-                      secondsToNiceTime(
-                        (rowData.playtime || 0) - (rowData.afkTime || 0)
-                      )
-                    }}
-                  </span>
-                </div>
-                <div
-                  class="bg-stone-600 h-full flex items-center pl-2"
-                  :style="{
-                    width:
-                      (rowData.playtime || 0) + (rowData.afkTime || 0) === 0
-                        ? '50%'
-                        : ((rowData.afkTime || 0) / (rowData.playtime || 0)) *
-                            100 +
-                          '%',
-                  }"
-                >
-                  <span class="truncate text-accent font-medium">
-                    AFK: {{ secondsToNiceTime(rowData.afkTime || 0) }}
-                  </span>
-                </div>
+      <div class="">
+        <Card>
+          <CardHeader>
+            <CardTitle> Online time </CardTitle>
+            <CardDescription>
+              {{ secondsToNiceTime(rowData.playtime || 0) }}
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div class="w-full flex h-8 rounded-lg overflow-hidden">
+              <div
+                class="bg-stone-700 h-full flex items-center pl-2"
+                :style="{
+                  width:
+                    (rowData.playtime || 0) + (rowData.afkTime || 0) === 0
+                      ? '50%'
+                      : (((rowData.playtime || 0) - (rowData.afkTime || 0)) /
+                          (rowData.playtime || 0)) *
+                          100 +
+                        '%',
+                }"
+              >
+                <span class="truncate text-accent font-medium">
+                  Active:
+                  {{
+                    secondsToNiceTime(
+                      (rowData.playtime || 0) - (rowData.afkTime || 0)
+                    )
+                  }}
+                </span>
               </div>
-            </CardContent>
-          </Card>
-          <div class="mt-4 flex gap-2">
-            <Badge variant="outline"
-              ><div class="flex items-center space-x-3">
-                <CoinsIcon class="size-4" />
-                <h1 class="font-medium">Wallet:</h1>
-                <p>{{ rowData.coins }} coins</p>
-              </div></Badge
-            >
-            <Badge variant="outline"
-              ><div class="flex items-center space-x-3">
-                <MedalIcon class="size-4" />
-                <h1 class="font-medium">Achievements:</h1>
-                <p>{{ rowData.achievements?.length || 0 }}</p>
-              </div></Badge
-            >
-            <Badge variant="outline"
-              ><div class="flex items-center space-x-3">
-                <ShirtIcon class="size-4" />
-                <h1 class="font-medium">Cosmetics:</h1>
-                <p>{{ rowData.cosmetics?.length || 0 }}</p>
-              </div></Badge
-            >
-          </div>
+              <div
+                class="bg-stone-600 h-full flex items-center pl-2"
+                :style="{
+                  width:
+                    (rowData.playtime || 0) + (rowData.afkTime || 0) === 0
+                      ? '50%'
+                      : ((rowData.afkTime || 0) / (rowData.playtime || 0)) *
+                          100 +
+                        '%',
+                }"
+              >
+                <span class="truncate text-accent font-medium">
+                  AFK: {{ secondsToNiceTime(rowData.afkTime || 0) }}
+                </span>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+        <div class="mt-4 flex gap-2">
+          <Badge variant="outline"
+            ><div class="flex items-center space-x-3">
+              <CoinsIcon class="size-4" />
+              <h1 class="font-medium">Wallet:</h1>
+              <p>{{ rowData.coins }} coins</p>
+            </div></Badge
+          >
+          <Badge variant="outline"
+            ><div class="flex items-center space-x-3">
+              <MedalIcon class="size-4" />
+              <h1 class="font-medium">Achievements:</h1>
+              <p>{{ rowData.achievements?.length || 0 }}</p>
+            </div></Badge
+          >
+          <Badge variant="outline"
+            ><div class="flex items-center space-x-3">
+              <ShirtIcon class="size-4" />
+              <h1 class="font-medium">Cosmetics:</h1>
+              <p>{{ rowData.cosmetics?.length || 0 }}</p>
+            </div></Badge
+          >
         </div>
-      </template>
+      </div>
     </DynaTable>
   </DashboardProvider>
 </template>
